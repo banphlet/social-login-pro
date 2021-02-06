c5d371795e6db2f0ebdaa4abb3021e4c4ad1d44d
banphlet <annor_theophilus@hotmail.com>
2021-02-06 17:27:53 +0000

    add ========>

--- server/services/platforms/wix/index.js
+++ server/services/platforms/wix/index.js
@@ -168,8 +168,8 @@ const fetchProducts = ({
     .then(response => response.body)
 
 const fetchAllProducts = async ({ accessToken, refreshToken, brand }) => {
-  console.log('fetching products accessToken', accessToken)
-  console.log('fetching products refreshToken', refreshToken)
+  console.log('fetching products accessToken========>', accessToken)
+  console.log('fetching products refreshToken========>', refreshToken)
 
   const firstItems = await fetchProducts({ accessToken, refreshToken })
   const allProducts = firstItems.products
