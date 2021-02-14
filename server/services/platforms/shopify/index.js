c0faea83d6bd107b9ff698f8c8674ba758a6cda4
banphlet <annor_theophilus@hotmail.com>
2021-02-14 21:57:45 +0000

    updated

--- server/services/platforms/shopify/index.js
+++ server/services/platforms/shopify/index.js
@@ -4,6 +4,12 @@ import Shopify from 'shopify-api-node'
 import config from '../../../config'
 import { asyncPipe, required } from '../../../lib/utils'
 import { Platforms } from '../../../models/shops/schema'
+import fs from 'fs'
+import util from 'util'
+
+const readFilePromise = util.promisify(fs.readFile)
+
+const renderSnippet = "{% render 'limit-login.liquid', form: form %}"
 
 const API_VERSION = '2020-04'
 
@@ -27,7 +33,13 @@ const shopifyToken = new ShopifyToken({
     'NEXT_PUBLIC_APP_URL'
   )}/api/permission-accepted/shopify`,
   apiKey: config.get('NEXT_PUBLIC_SHOPIFY_CLIENT_ID'),
-  scopes: ['read_content', 'read_script_tags', 'write_script_tags']
+  scopes: [
+    'read_content',
+    'read_script_tags',
+    'write_script_tags',
+    'read_themes',
+    'write_themes'
+  ]
 })
 
 const getPermissionUrl = ({ shop = required('shop') }) =>
@@ -58,18 +70,58 @@ const getSiteDetails = ({
       externalId: shopDetails.id
     }))
 
-const injectScript = ({
+const insertSnippetIntoCurrentLiquid = (value = '') => {
+  const snippet = "{% render 'limit-login.liquid', form: form %}"
+  const match = value.match(/customer_login.*}/)
+  if (!match) return value
+  const indexAfterFormTag = match.index + match[0].length
+  return (
+    value.slice(0, indexAfterFormTag) + snippet + value.slice(indexAfterFormTag)
+  )
+}
+const loadScript = async shopId => {
+  const script = (
+    await readFilePromise(process.cwd() + '/script.liquid')
+  ).toString('utf-8')
+
+  return script
+    .replace('SHOP_ID', shopId)
+    .replace('NEXT_PUBLIC_APP_URL', config.get('NEXT_PUBLIC_APP_URL'))
+}
+
+const injectScript = async ({
   accessToken = required('accessToken'),
   platformDomain = required('platformDomain'),
   shopId = required('shopId')
-}) =>
-  shopifyClient({ shop: platformDomain, accessToken })
-    .scriptTag.create({
-      src: `${config.get('NEXT_PUBLIC_APP_URL')}/js/lla.js?shop_id=${shopId}`,
-      event: 'onload'
-    })
-    .catch(err => {
-      console.log(err)
-    })
+}) => {
+  const loginTemplateBasePath = 'templates/customers/login'
+  const shopify = shopifyClient({ shop: platformDomain, accessToken })
+  const publishedTheme = (await shopify.theme.list()).find(
+    theme => theme.role === 'main'
+  )
+
+  const customerLoginAsset = await shopify.asset.get(publishedTheme.id, {
+    'asset[key]': `${loginTemplateBasePath}.liquid`
+  })
+
+  const hasSnippet = new RegExp(renderSnippet).test(customerLoginAsset.value)
+  if (hasSnippet) return
+
+  // backup customer login file
+  await shopify.asset.create(publishedTheme.id, {
+    key: `${loginTemplateBasePath}-backup.liquid`,
+    value: customerLoginAsset.value
+  })
+
+  await shopify.asset.create(publishedTheme.id, {
+    key: 'snippets/limit-login.liquid',
+    value: await loadScript(shopId)
+  })
+
+  await shopify.asset.update(publishedTheme.id, {
+    key: `${loginTemplateBasePath}.liquid`,
+    value: insertSnippetIntoCurrentLiquid(customerLoginAsset.value)
+  })
+}
 
 export { getPermissionUrl, getOauthAccessTokens, getSiteDetails, injectScript }
