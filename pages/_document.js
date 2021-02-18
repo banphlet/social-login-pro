4848b191a663dbac42f72f0cddeaf6f05ea3761f
banphlet <annor_theophilus@hotmail.com>
2021-02-18 21:30:03 +0000

    add live chat

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  render () {
+  render() {
     return (
       <Html>
         <Head />
@@ -12,6 +12,7 @@ class MyDocument extends Document {
           src='https://connect.facebook.net/en_US/all.js'
         ></script>
         <script src='//static.parastorage.com/services/js-sdk/1.537.0/js/wix.min.js'></script>
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/6521527.js"></script>
         <body>
           <Main />
           <NextScript />
