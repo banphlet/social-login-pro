ee4db5b228cd07e12b5c62dbb16ef60c6b00df82
banphlet <annor_theophilus@hotmail.com>
2021-02-23 16:45:29 +0000

    updated

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,9 @@ import Settings from '../Components/Settings'
 import { AppProvider, Frame } from '@shopify/polaris'
 import enTranslations from '@shopify/polaris/locales/en.json'
 import { Provider, useAppBridge } from '@shopify/app-bridge-react'
-import { TitleBar, Button, ButtonGroup } from '@shopify/app-bridge/actions';
+import { TitleBar, Button } from '@shopify/app-bridge/actions'
 import SocialLogin from '../Components/SocialLogin'
 import useMutation from '../Hooks/useMutation'
-
 
 const tabs = [
   {
@@ -29,52 +28,69 @@ const tabs = [
   }
 ]
 
-
 const Main = ({ shop }) => {
   const [selected, setSelected] = React.useState(0)
   const { makeRequest, loading, data: { data } = {} } = useMutation({
     path: 'shops/me',
     method: 'put'
   })
-  const app = useAppBridge();
+  const app = useAppBridge()
 
   React.useEffect(() => {
     showPlan()
   }, [])
 
   const showPlan = () => {
-    const saveButton = Button.create(app, { label: shop.plan?.name, style: Button.Style.Danger });
+    const saveButton = Button.create(app, {
+      label: shop.plan?.name,
+      style: Button.Style.Danger
+    })
     TitleBar.create(app, {
       buttons: {
-        secondary: [saveButton],
+        secondary: [saveButton]
       }
-    });
+    })
   }
-
 
   const handleTabChange = React.useCallback(
     selectedTabIndex => setSelected(selectedTabIndex),
     []
   )
-  return <Frame>
-    <div style={{ padding: 5 }}>
-      <Card>
-        <Tabs
-          tabs={tabs}
-          selected={selected}
-          onSelect={handleTabChange}
-          disclosureText='More views'
-        >
-          {selected === 0 && <Analytics shop={shop} />}
-          {selected === 1 && <Settings makeRequest={makeRequest} loading={loading} data={data} shop={shop} />}
-          {selected === 2 && <SocialLogin makeRequest={makeRequest} loading={loading} data={data} shop={shop} />}
-        </Tabs>
-      </Card>
-    </div>
-  </Frame>
+  return (
+    <Frame>
+      <div style={{ padding: 5 }}>
+        <Card>
+          <Tabs
+            tabs={tabs}
+            selected={selected}
+            onSelect={handleTabChange}
+            disclosureText='More views'
+          >
+            {selected === 0 && <Analytics shop={shop} />}
+            {selected === 1 && (
+              <Settings
+                makeRequest={makeRequest}
+                loading={loading}
+                data={data}
+                shop={shop}
+              />
+            )}
+            {selected === 2 && (
+              <SocialLogin
+                makeRequest={makeRequest}
+                loading={loading}
+                data={data}
+                shop={shop}
+              />
+            )}
+          </Tabs>
+        </Card>
+      </div>
+    </Frame>
+  )
 }
 
-export default function Home({ shop }) {
+export default function Home ({ shop }) {
   return (
     <AppProvider i18n={enTranslations}>
       <Provider
