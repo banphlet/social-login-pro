df8f083ffe6f93b3afd7e3746ccc9a5943dd65a9
Nana Kwame Zoe <annor_theophilus@hotmail.com>
2021-10-10 16:34:15 +0000

    allow

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const { i18n } = require('./next-i18next.config.js')
 
 module.exports = {
   i18n,
-  headers () {
+  async headers () {
     return [
       {
-        source: '*',
+        source: '/*',
         headers: [
           {
             key: 'x-frame-options',
